7ae4daccd14ca75de30aed017b8ce9b9be97387a
Mika <66172894+mikaoes@users.noreply.github.com>
2023-04-24 09:55:06 +0000

    initial commit

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
+import requests
 
+url = "https://witzapi.de/api/joke/?language=de"
+
+def request():
+    r = requests.get(url)
+    json = r.json()
+    return json[0]["text"]
+
+print(request())
